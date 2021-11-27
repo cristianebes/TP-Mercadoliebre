fb717dfc92bf4158a74ddc98fdb99c8e802a32d0
cristianebes <cristian.besada@hotmail.com>
2021-11-27 17:43:11 -0300

    Version inicial del proyecto Mercadoliebre

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static('public'));
+
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/views/home.html")));
+
+app.get("/register", (req, res) => res.sendFile(path.join(__dirname, "/views/register.html")));
+
+app.get("/login", (req, res) => res.sendFile(path.join(__dirname, "/views/login.html")));
+
+app.post('/register', (req, res) => res.redirect(302, '/'));
+
+app.post('/login', (req, res) => res.redirect(302, '/'));
+
+app.listen(3001, () => console.log("Pagina corriendo"));
+
---
+++ readme.md
@@ -0,0 +1,16 @@
+# Ejercicio - MercadoLiebre
+
+Welcome to MercadoLiebre!
+
+* [Step by step][step_by_step].
+
+## Step by step
+
+```shell
+A. "git clone https://github.com/cristianebes/TP-Mercadoliebre.git" to download the files
+B. "cd MercadoLiebre" to get into the folder
+C. "npm install" to download Express and Nodemon
+D. Run the code and check our website
+```
+
+[step_by_step]: ##step-by-step
